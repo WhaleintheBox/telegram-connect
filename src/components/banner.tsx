56cf56edd221ec6acd9341d72f46cbe86e3788df
Romain <romainborelli@MacBook-Pro-de-Romain-2.local>
2024-12-17 02:58:07 +0100

    push

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -134,11 +134,11 @@ export const Banner = () => {
       const root = document.documentElement;
   
       if (screenWidth > 1024) {
-        root.style.setProperty('--ticker-duration', '60s'); // Desktop
+        root.style.setProperty('--ticker-duration', '120s'); // Desktop
       } else if (screenWidth > 768) {
-        root.style.setProperty('--ticker-duration', '40s'); // Tablettes
+        root.style.setProperty('--ticker-duration', '90s'); // Tablettes
       } else {
-        root.style.setProperty('--ticker-duration', '30s'); // Mobile
+        root.style.setProperty('--ticker-duration', '60s'); // Mobile
       }
     };
   
